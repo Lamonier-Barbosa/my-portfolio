6d1a5dc29dfea54b0a3fc036194c34a9a78a23be
Lamonier Barbosa <lamoni_lr@hotmail.com>
2024-10-01 19:33:06 -0300

    Adicionando animações e textos reais ao projeto

--- js/script.js
+++ js/script.js
@@ -24,3 +24,47 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+
+// CONTATO
+const button = document.getElementById('btn-form');
+
+const enviar = () => {
+    button.innerHTML = 'Enviar'
+}
+
+const addLoading = () => {
+    button.style.background = 'black';
+    button.style.border = '2px solid var(--main - color)';
+    button.innerHTML = '<img src="./assets/svg/loading.png" alt="SVG de loading" class="loading" id="loading">';
+};
+
+const removeLoading = () => {
+    button.style.background = 'var(--main-color)';
+    button.style.border = '2px solid transparent';
+    button.innerHTML = '<img src="./assets/svg/check.svg" alt="SVG de loading" class="loading" id = "loading" style = "animation: none" > ';
+};
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    addLoading();
+
+    const name = document.querySelector('input[name=Nome]').value;
+    const email = document.querySelector('input[name=Email]').value;
+    const whatsapp = document.querySelector('input[name=WhatsApp]').value;
+    const sobre = document.querySelector('input[name=Sobre]').value;
+    const mensagem = document.querySelector('textarea[name=Mensagem]').value;
+
+    fetch('https://api.sheetmonkey.io/form/58UUSyNjo15oDQs8yZf2S2', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'content-type': 'application/json',
+        },
+
+        body: JSON.stringify({ name, email, whatsapp, sobre, mensagem }),
+    }).then(() => {
+        removeLoading();
+    });
+}
+
+document.querySelector('form').addEventListener('submit', handleSubmit)
